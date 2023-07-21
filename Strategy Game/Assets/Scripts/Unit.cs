f3921ea3cc2ad0387c9134ee8db11c04c1699cf3
JoelSuperHacker <09.joelandersson@gmail.com>
2023-07-21 16:12:45 +0200

    started on stuff... its broken

---
+++ Strategy Game/Assets/Scripts/Unit.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        UnitSelections.Instance.unitList.Add(this.gameObject);
+    }
+
+    
+
+    void OnDestroy()
+    {
+        UnitSelections.Instance.unitList.Remove(this.gameObject);
+    }
+}
